ff25f4d038f92a06a7ac81704ffba5446cb1585e
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-10-16 19:13:12 +0300

    добавил стартовые файлы

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Container from './Container';
+
+function App() {
+  return (
+    <Container>
+      <h1>Movies</h1>
+    </Container>
+  );
+}
+
+export default App;
