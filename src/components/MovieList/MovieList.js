5d603c0df0ac19dd7d07448234ceb710ca03861c
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-10-23 17:39:20 +0300

    добавлена кнопка назад

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,15 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function MovieList({ movies }) {
-  // const { url } = useRouteMatch();
-
-  // console.log(url);
+  const location = useLocation();
 
   return (
     <ul style={{ marginTop: '20px' }}>
       {movies.map(movie => (
         <li key={movie.id} style={{ marginBottom: '5px' }}>
-          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          <Link
+            to={{
+              pathname: `/movies/${movie.id}`,
+              state: {
+                from: location,
+              },
+            }}
+          >
+            {movie.title}
+          </Link>
         </li>
       ))}
     </ul>
--- src/views/MovieDetailsPage.js
+++ src/views/MovieDetailsPage.js
@@ -1,5 +1,12 @@
 import { useState, useEffect, lazy, Suspense } from 'react';
-import { useParams, useRouteMatch, Link, Route } from 'react-router-dom';
+import {
+  useParams,
+  useRouteMatch,
+  Link,
+  Route,
+  useHistory,
+  useLocation,
+} from 'react-router-dom';
 import Loader from 'react-loader-spinner';
 import { toast } from 'react-toastify';
 
@@ -19,6 +26,8 @@ export default function MovieDetailsPage() {
   const [showLoader, setShowLoader] = useState(false);
   const { movieId } = useParams();
   const { url } = useRouteMatch();
+  const history = useHistory();
+  const location = useLocation();
 
   useEffect(() => {
     async function fetchMovieDetails() {
@@ -37,6 +46,10 @@ export default function MovieDetailsPage() {
     fetchMovieDetails();
   }, [movieId]);
 
+  const onGoBack = () => {
+    history.push(location?.state?.from ?? '/');
+  };
+
   return (
     <>
       {showLoader && (
@@ -49,62 +62,72 @@ export default function MovieDetailsPage() {
           style={{ textAlign: 'center' }}
         />
       )}
+
       {movie && (
-        <div>
-          <div className={styles.Wrapper}>
-            <div>
-              <img
-                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                alt={movie.title}
-                className={styles.Image}
-              />
-            </div>
-            <div className={styles.Description}>
-              <h2>
-                Title: {movie.title}{' '}
-                <span>({movie.release_date.split('-')[0]})</span>
-              </h2>
-              <h2>Overview:</h2>
-              <p>{movie.overview}</p>
-              <h3>Popularity:</h3>
-              <span>{movie.popularity}</span>
-              <h3>Genres:</h3>
-              <span>{movie.genres?.map(({ name }) => name).join(', ')}</span>
-            </div>
-          </div>
-          <h2>Additional information:</h2>
-          <ul>
-            <li>
-              <Link to={`${url}/cast`}>
-                <h3>Cast</h3>
-              </Link>
-            </li>
-            <li>
-              <Link to={`${url}/reviews`}>
-                <h3>Reviews</h3>
-              </Link>
-            </li>
-          </ul>
-          <Suspense
-            fallback={
-              <Loader
-                type="Oval"
-                color="#00BFFF"
-                height={80}
-                width={80}
-                timeout={3000}
-                style={{ textAlign: 'center' }}
-              />
-            }
+        <>
+          <button
+            type="button"
+            style={{ marginBottom: '20px' }}
+            onClick={onGoBack}
           >
-            <Route path={`${url}/cast`}>
-              <Cast movieId={movie.id} />
-            </Route>
-            <Route path={`${url}/reviews`}>
-              <Reviews movieId={movie.id} />
-            </Route>
-          </Suspense>
-        </div>
+            Go back
+          </button>
+          <div>
+            <div className={styles.Wrapper}>
+              <div>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                  alt={movie.title}
+                  className={styles.Image}
+                />
+              </div>
+              <div className={styles.Description}>
+                <h2>
+                  Title: {movie.title}{' '}
+                  <span>({movie.release_date.split('-')[0]})</span>
+                </h2>
+                <h2>Overview:</h2>
+                <p>{movie.overview}</p>
+                <h3>Popularity:</h3>
+                <span>{movie.popularity}</span>
+                <h3>Genres:</h3>
+                <span>{movie.genres?.map(({ name }) => name).join(', ')}</span>
+              </div>
+            </div>
+            <h2>Additional information:</h2>
+            <ul>
+              <li>
+                <Link to={`${url}/cast`}>
+                  <h3>Cast</h3>
+                </Link>
+              </li>
+              <li>
+                <Link to={`${url}/reviews`}>
+                  <h3>Reviews</h3>
+                </Link>
+              </li>
+            </ul>
+            <Suspense
+              fallback={
+                <Loader
+                  type="Oval"
+                  color="#00BFFF"
+                  height={80}
+                  width={80}
+                  timeout={3000}
+                  style={{ textAlign: 'center' }}
+                />
+              }
+            >
+              <Route path={`${url}/cast`}>
+                <Cast movieId={movie.id} />
+              </Route>
+              <Route path={`${url}/reviews`}>
+                <Reviews movieId={movie.id} />
+              </Route>
+            </Suspense>
+          </div>
+        </>
       )}
     </>
   );
